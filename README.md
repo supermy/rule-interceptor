87b2d4bc406df4815f663fa0eebc06906b239e77
supermy <springclick@gmail.com>
2016-12-16 10:32:33 +0800

    完成flume 正则拦截器的替换
    Search and Replace Interceptor；--> RuleSearchAndReplaceInterceptor$Builder
    Regex Filtering Interceptor；  -->RuleFilteringInterceptor$Builder
    Regex Extractor Interceptor；-->RuleSearchAndReplaceInterceptor$Builder

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
 # key-switch
+2016-12-16 完成flume 正则拦截器的替换
+
+Search and Replace Interceptor；--> RuleSearchAndReplaceInterceptor$Builder
+Regex Filtering Interceptor；  -->RuleFilteringInterceptor$Builder
+Regex Extractor Interceptor；-->RuleSearchAndReplaceInterceptor$Builder
+---------------------filter dsl
+println head
+println body
+
+return true
+
+---------------------search replace dsl
+println head
+println body
+body=body.replace('a','aaa')
+head["newhead"]='abcd'
+
+def resultMap = [:]
+
+resultMap["head"]=head
+resultMap["body"]=body
+
+return resultMap
+
+
+
+a1.sources.r1.interceptors = i1 i2 i3
+a1.sources.r1.interceptors.i1.type = regex_extractor
+a1.sources.r1.interceptors.i1.regex = (.+):(.+):(.+):(.+)
+a1.sources.r1.interceptors.i1.serializers = s1 s2 s3 s4
+a1.sources.r1.interceptors.i1.serializers.s1.name = routing-key
+a1.sources.r1.interceptors.i1.serializers.s2.name = starttime
+a1.sources.r1.interceptors.i1.serializers.s3.name = endtime
+a1.sources.r1.interceptors.i1.serializers.s4.name = systime
+
+#如果excludeEvents设为true，则表示过滤掉以lxw1234开头的events。
+#在使用 Regex Filtering Interceptor的时候一个属性是excludeEvents
+#当它的值为true 的时候，过滤掉匹配到当前正则表达式的一行
+#当它的值为false的时候，就接受匹配到正则表达式的一行
+
+#a1.sources.r1.interceptors = i2
+a1.sources.r1.interceptors.i2.type = com.supermy.flume.interceptor.RuleFilteringInterceptor$Builder
+#通过脚本的返回值，数据是否向下传递 参数 body / head(json 格式) 可以引用
+a1.sources.r1.interceptors.i2.rule = /etc/flume/conf/g-filter.groovy
+a1.sources.r1.interceptors.i2.ruleName = filterGroovy
+
+a1.sources.r1.interceptors.i3.type = com.supermy.flume.interceptor.RuleSearchAndReplaceInterceptor$Builder
+#通过脚本的返回值，数据是否向下传递 参数 body / head(json 格式) 可以引用
+a1.sources.r1.interceptors.i3.searchReplaceDsl = /etc/flume/conf/g-search-replace.groovy
+a1.sources.r1.interceptors.i3.searchReplaceKey = searchReplaceGroovy
+
+
 2016-12-14 完成拦截器项目的初始化
 自定义拦截器，通用的groovy 脚本语言作为规则语音。
---
+++ src/main/java/com/supermy/flume/interceptor/RuleSearchAndReplaceInterceptor.java
@@ -0,0 +1,192 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.supermy.flume.interceptor;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Preconditions;
+import groovy.lang.Binding;
+import org.apache.commons.lang.StringUtils;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.interceptor.Interceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * <p>
+ * Interceptor that allows search-and-replace of event body strings using
+ * regular expressions. This only works with event bodies that are valid
+ * strings. The charset is configurable.
+ * <p>
+ * Usage:
+ * <pre>
+ *   agent.source-1.interceptors.search-replace.searchPattern = ^INFO:
+ *   agent.source-1.interceptors.search-replace.replaceString = Log msg:
+ * </pre>
+ * <p>
+ * Any regular expression search pattern and replacement pattern that can be
+ * used with {@link Matcher#replaceAll(String)} may be used,
+ * including backtracking and grouping.
+ */
+
+/**
+ * 搜索与替换
+ * 搜索改用groovy 脚本进行；
+ * 替换改用groovy 脚本进行；
+ *
+ * 从boddy 中抽取数据，放到head中；
+ *
+ *  <p>
+ * Usage:
+ * <pre>
+ *   agent.source-1.interceptors.search-replace.searchReplaceKey = GroovyDSLName
+ *   agent.source-1.interceptors.search-replace.searchReplaceDsl = GroovyDSLFile:
+ * </pre>
+ * <p>
+ *
+ */
+public class RuleSearchAndReplaceInterceptor implements Interceptor {
+
+  private static final Logger logger = LoggerFactory
+      .getLogger(RuleSearchAndReplaceInterceptor.class);
+
+  private final String searchReplaceKey;
+  private final String searchReplaceDsl;
+
+
+//  private final Pattern searchPattern;
+//  private final String replaceString;
+
+  private final Charset charset;
+
+//  private RuleSearchAndReplaceInterceptor(Pattern searchPattern,
+//                                      String replaceString,
+//                                      Charset charset) {
+//    this.searchPattern = searchPattern;
+//    this.replaceString = replaceString;
+//    this.charset = charset;
+//  }
+private RuleSearchAndReplaceInterceptor(String searchReplaceKey,String searchReplaceDsl,
+                                        Charset charset) {
+  this.searchReplaceKey=searchReplaceKey;
+  this.searchReplaceDsl=searchReplaceDsl;
+  this.charset = charset;
+}
+
+
+  @Override
+  public void initialize() {
+  }
+
+  @Override
+  public void close() {
+  }
+
+//  @Override
+//  public Event intercept(Event event) {
+//    String origBody = new String(event.getBody(), charset);
+//    Matcher matcher = searchPattern.matcher(origBody);
+//    String newBody = matcher.replaceAll(replaceString);
+//    event.setBody(newBody.getBytes(charset));
+//    return event;
+//  }
+  @Override
+  public Event intercept(Event event) {
+    String origBody = new String(event.getBody(), charset);
+    Map<String, String> headers = event.getHeaders();
+
+    //输入参数
+    Binding binding = new Binding();
+    binding.setVariable("body", origBody);
+    binding.setVariable("head", headers);
+    //查找匹配数据；
+    File f =new File(searchReplaceDsl);
+    Map result =(Map) GroovyShellJsonExample.getShell(searchReplaceKey+f.lastModified(), f, binding);
+
+    //替换匹配数据；
+
+//    Matcher matcher = searchPattern.matcher(origBody);
+//    String newBody = matcher.replaceAll(replaceString);
+    logger.debug(result.get("body").toString());
+    logger.debug(result.get("head").toString());
+
+    event.setBody(result.get("body").toString().getBytes(charset));
+    event.setHeaders((Map)result.get("head"));
+    return event;
+  }
+
+  @Override
+  public List<Event> intercept(List<Event> events) {
+    for (Event event : events) {
+      intercept(event);
+    }
+    return events;
+  }
+
+  public static class Builder implements Interceptor.Builder {
+
+
+
+    private static final String SEARCH_REPLACE_KEY = "searchReplaceKey";
+    private static final String SEARCH_REPLACE_DSL = "searchReplaceDsl";
+    private static final String CHARSET_KEY = "charset";
+
+    private  String searchReplaceKey;
+    private  String searchReplaceDsl;
+
+//    private Pattern searchRegex;
+//    private String replaceString;
+    private Charset charset = Charsets.UTF_8;
+
+    @Override
+    public void configure(Context context) {
+      searchReplaceKey = context.getString(SEARCH_REPLACE_KEY);
+      Preconditions.checkArgument(!StringUtils.isEmpty(searchReplaceKey),
+          "Must supply a valid search pattern " + SEARCH_REPLACE_KEY +
+          " (may not be empty)");
+
+      searchReplaceDsl = context.getString(SEARCH_REPLACE_DSL);
+      Preconditions.checkNotNull(searchReplaceDsl,
+          "Must supply a replacement string " + SEARCH_REPLACE_DSL +
+          " (empty is ok)");
+
+//      searchRegex = Pattern.compile(searchPattern);
+
+      if (context.containsKey(CHARSET_KEY)) {
+        // May throw IllegalArgumentException for unsupported charsets.
+        charset = Charset.forName(context.getString(CHARSET_KEY));
+      }
+    }
+
+    @Override
+    public Interceptor build() {
+      Preconditions.checkNotNull(searchReplaceKey,
+                                 "searchReplaceKey required");
+      Preconditions.checkNotNull(searchReplaceDsl,
+                                 "searchReplaceDsl required");
+      return new RuleSearchAndReplaceInterceptor(searchReplaceKey, searchReplaceDsl, charset);
+    }
+  }
+}
